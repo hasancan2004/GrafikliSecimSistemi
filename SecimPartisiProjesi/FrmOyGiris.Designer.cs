ba46b067cf02d044b55598bc48cdb38415dae10d
Hasan Can KULA <hasancan.kula0707@gmail.com>
2024-07-30 20:07:00 +0300

    SecimPartisiProjesi is created

---
+++ SecimPartisiProjesi/FrmOyGiris.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace SecimPartisiProjesi
+{
+    partial class FrmOyGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtilceAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtD = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtE = new System.Windows.Forms.TextBox();
+            this.BtnOyGiris = new System.Windows.Forms.Button();
+            this.BtnGrafikler = new System.Windows.Forms.Button();
+            this.BtnCikisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İLÇE AD:";
+            // 
+            // TxtilceAd
+            // 
+            this.TxtilceAd.Location = new System.Drawing.Point(223, 42);
+            this.TxtilceAd.Name = "TxtilceAd";
+            this.TxtilceAd.Size = new System.Drawing.Size(355, 30);
+            this.TxtilceAd.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "A PARTİ:";
+            // 
+            // TxtA
+            // 
+            this.TxtA.Location = new System.Drawing.Point(223, 123);
+            this.TxtA.Name = "TxtA";
+            this.TxtA.Size = new System.Drawing.Size(355, 30);
+            this.TxtA.TabIndex = 1;
+            this.TxtA.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "B PARTİ:";
+            // 
+            // TxtB
+            // 
+            this.TxtB.AcceptsReturn = true;
+            this.TxtB.Location = new System.Drawing.Point(223, 174);
+            this.TxtB.Name = "TxtB";
+            this.TxtB.Size = new System.Drawing.Size(355, 30);
+            this.TxtB.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "C PARTİ:";
+            // 
+            // TxtC
+            // 
+            this.TxtC.Location = new System.Drawing.Point(223, 222);
+            this.TxtC.Name = "TxtC";
+            this.TxtC.Size = new System.Drawing.Size(355, 30);
+            this.TxtC.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "D PARTİ:";
+            // 
+            // TxtD
+            // 
+            this.TxtD.Location = new System.Drawing.Point(223, 279);
+            this.TxtD.Name = "TxtD";
+            this.TxtD.Size = new System.Drawing.Size(355, 30);
+            this.TxtD.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 334);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "E PARTİ:";
+            // 
+            // TxtE
+            // 
+            this.TxtE.Location = new System.Drawing.Point(223, 343);
+            this.TxtE.Name = "TxtE";
+            this.TxtE.Size = new System.Drawing.Size(355, 30);
+            this.TxtE.TabIndex = 5;
+            // 
+            // BtnOyGiris
+            // 
+            this.BtnOyGiris.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BtnOyGiris.Location = new System.Drawing.Point(223, 395);
+            this.BtnOyGiris.Name = "BtnOyGiris";
+            this.BtnOyGiris.Size = new System.Drawing.Size(355, 39);
+            this.BtnOyGiris.TabIndex = 6;
+            this.BtnOyGiris.Text = "OY GİRİŞİ YAP";
+            this.BtnOyGiris.UseVisualStyleBackColor = false;
+            this.BtnOyGiris.Click += new System.EventHandler(this.BtnOyGiris_Click);
+            // 
+            // BtnGrafikler
+            // 
+            this.BtnGrafikler.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BtnGrafikler.Location = new System.Drawing.Point(223, 440);
+            this.BtnGrafikler.Name = "BtnGrafikler";
+            this.BtnGrafikler.Size = new System.Drawing.Size(191, 62);
+            this.BtnGrafikler.TabIndex = 7;
+            this.BtnGrafikler.Text = "GRAFİKLER";
+            this.BtnGrafikler.UseVisualStyleBackColor = false;
+            this.BtnGrafikler.Click += new System.EventHandler(this.BtnGrafikler_Click);
+            // 
+            // BtnCikisYap
+            // 
+            this.BtnCikisYap.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BtnCikisYap.Location = new System.Drawing.Point(420, 440);
+            this.BtnCikisYap.Name = "BtnCikisYap";
+            this.BtnCikisYap.Size = new System.Drawing.Size(158, 62);
+            this.BtnCikisYap.TabIndex = 8;
+            this.BtnCikisYap.Text = "ÇIKIŞ YAP";
+            this.BtnCikisYap.UseVisualStyleBackColor = false;
+            // 
+            // FrmOyGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(889, 527);
+            this.Controls.Add(this.BtnCikisYap);
+            this.Controls.Add(this.BtnGrafikler);
+            this.Controls.Add(this.BtnOyGiris);
+            this.Controls.Add(this.TxtE);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtilceAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmOyGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VTGS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtilceAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtE;
+        private System.Windows.Forms.Button BtnOyGiris;
+        private System.Windows.Forms.Button BtnGrafikler;
+        private System.Windows.Forms.Button BtnCikisYap;
+    }
+}
+
